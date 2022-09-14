56c054322ffe5254dd153ef0661a47a06ebe5c51
tvthieu <tranthieu8686@gmail.com>
2022-09-14 20:01:59 +0700

    jwt authenticate login and register

--- models/user.js
+++ models/user.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
-var UserSchema = new Schema({
-    name: {
+
+// create model user
+const UserSchema = new Schema({
+    fullName: {
         type: String,
         required: true
     },
@@ -17,12 +20,39 @@ var UserSchema = new Schema({
     },
     gender: {
         type: String,
-        required: false
+        default: null
     },
     avatarUrl: {
         type: String,
-        required: false
+        default: null
+    },
+    birth_date: { type: String, default: null },
+    phone: { type: Number, default: null },
+    created: {
+        type: Date,
+        default: Date.now
     }
 });
+//This is called a pre-hook, before the user information is saved in the database
+//this function will be called, we'll get the plain text password, hash it and store it.
+UserSchema.pre('save', async function (next) {
+    //'this' refers to the current document about to be saved
+    const user = this;
+    //Hash the password with a salt round of 10, the higher the rounds the more secure, but the slower
+    //your application becomes.
+    const hash = await bcrypt.hash(this.password, 10);
+    //Replace the plain text password with the hash and then store it
+    this.password = hash;
+    //Indicates we're done and moves on to the next middleware
+    next();
+});
 
-module.exports= mongoose.model('User', UserSchema);
+//We'll use this later on to make sure that the user trying to log in has the correct credentials
+UserSchema.methods.isValidPassword = async function (password) {
+    const user = this;
+    //Hashes the password sent by the user for login and checks if the hashed password stored in the
+    //database matches the one sent. Returns true if it does else false.
+    const compare = await bcrypt.compare(password, user.password);
+    return compare;
+}
+module.exports = mongoose.model('User', UserSchema);
